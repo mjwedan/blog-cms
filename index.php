3cedcd8a84e61bade90448b7b212e6bb59b80668
mjwedan <mwedan@lesley.edu>
2021-08-01 17:47:46 -0500

    Updated index and post to include php functions

--- index.php
+++ index.php
@@ -7,8 +7,22 @@
         <?php include 'header.php' ?>
         <?php include 'nav.php' ?>
         <main>
+        <?php
+            function getPostTitlesFromDatabase() {
+                // TODO in Module 4
+                // get this data from a database instead of hardcoding it
+                $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
+                return $postTitles;
+            }
+        ?>
             <ul>
-                <li><a href="post.php">BLOG POST 1</a></li>
+                <?php 
+                    $postTitles = getPostTitlesFromDatabase();
+
+                    foreach($postTitles as $postTitle) {
+                        echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
+                    }
+                ?>
             </ul>
         </main>
         <?php include 'footer.php' ?>
--- post.php
+++ post.php
@@ -7,13 +7,26 @@
         <?php include 'header.php' ?>
         <?php include 'nav.php' ?>
         <main>
-            <h2>Title</h2>
-            <div>Author</div>
-            <div>Date Published</div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, skateboard locavore farm-to-table street art, schlitz flexitarian health goth. Cold-pressed next level heirloom, hexagon crucifix banjo photo booth. Intelligentsia XOXO lumbersexual mumblecore venmo single-origin coffee pug cloud bread hot chicken kinfolk +1 tilde slow-carb echo park. Asymmetrical succulents artisan, you probably haven't heard of them gluten-free austin pabst fixie scenester pinterest neutra cloud bread.</p>
-                <p>Shaman vape bespoke migas hot chicken locavore art party pitchfork chicharrones prism hoodie post-ironic bushwick pop-up. Hexagon four dollar toast bicycle rights vice tacos austin locavore church-key, wolf food truck whatever cardigan disrupt flannel selvage. Brooklyn banh mi man bun la croix before they sold out street art lomo chia ugh fingerstache cloud bread neutra irony coloring book.</p>
-            </div>
+            <?php
+                    function getPostDetailsFromDatabase() {
+                        // TODO in Module 4
+                        // get this data from a database instead of hardcoding it
+                        $postDetails = array('title' => 'Blog Post 1', 
+                                            'date' => '08/01/2021',
+                                            'author' => 'mjwedan',
+                                            'content' => '<p>Lorem ipsum dolor sit amet, skateboard locavore farm-to-table street art, schlitz flexitarian health goth. Cold-pressed next level heirloom, hexagon crucifix banjo photo booth. Intelligentsia XOXO lumbersexual mumblecore venmo single-origin coffee pug cloud bread hot chicken kinfolk +1 tilde slow-carb echo park. Asymmetrical succulents artisan, you probably havent heard of them gluten-free austin pabst fixie scenester pinterest neutra cloud bread.</p>
+                                            <p>Shaman vape bespoke migas hot chicken locavore art party pitchfork chicharrones prism hoodie post-ironic bushwick pop-up. Hexagon four dollar toast bicycle rights vice tacos austin locavore church-key, wolf food truck whatever cardigan disrupt flannel selvage. Brooklyn banh mi man bun la croix before they sold out street art lomo chia ugh fingerstache cloud bread neutra irony coloring book.</p>');
+                        return $postDetails;
+                    }
+            ?>
+            <?php  
+                    // Post details contains all the data to generate the blog from
+                    $postDetails = getPostDetailsFromDatabase();
+                ?>
+                <h2> <?php echo $postDetails["title"]; ?> </h2>
+                <div> <?php echo $postDetails["author"]; ?></div>
+                <div> <?php echo $postDetails["date"]; ?></div>
+                <div> <?php echo $postDetails["content"]; ?></div>
         </main>
         <?php include 'footer.php' ?>
     </body>
