93a0146b3becb93bf590853b75622429f252754b
mjwedan <mwedan@lesley.edu>
2021-08-07 00:14:09 -0500

    Editing the php to start pulling the details of the blog posts from the actual database instead of hard-coding them

--- post.php
+++ post.php
@@ -9,13 +9,16 @@
         <main>
             <?php
                     function getPostDetailsFromDatabase() {
-                        // TODO in Module 4
-                        // get this data from a database instead of hardcoding it
-                        $postDetails = array('title' => 'Blog Post 1', 
-                                            'date' => '08/01/2021',
-                                            'author' => 'mjwedan',
-                                            'content' => '<p>Lorem ipsum dolor sit amet, skateboard locavore farm-to-table street art, schlitz flexitarian health goth. Cold-pressed next level heirloom, hexagon crucifix banjo photo booth. Intelligentsia XOXO lumbersexual mumblecore venmo single-origin coffee pug cloud bread hot chicken kinfolk +1 tilde slow-carb echo park. Asymmetrical succulents artisan, you probably havent heard of them gluten-free austin pabst fixie scenester pinterest neutra cloud bread.</p>
-                                            <p>Shaman vape bespoke migas hot chicken locavore art party pitchfork chicharrones prism hoodie post-ironic bushwick pop-up. Hexagon four dollar toast bicycle rights vice tacos austin locavore church-key, wolf food truck whatever cardigan disrupt flannel selvage. Brooklyn banh mi man bun la croix before they sold out street art lomo chia ugh fingerstache cloud bread neutra irony coloring book.</p>');
+                        // Get the post title
+                        $postTitle = rawurldecode($_GET["title"]);
+
+                        // Get the post that matches the postTitle
+                        include_once 'db_connect.php';
+                        $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
+                        $result = mysqli_query($conn, $sql);
+
+                        // Get the first row from the result as an associative array
+                        $postDetails = mysqli_fetch_assoc($result);
                         return $postDetails;
                     }
             ?>
